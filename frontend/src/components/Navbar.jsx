832e6d25075f0f18d313e98c7ce60da64065e2dd
Sahil <omanwarsahil@gmail.com>
2024-09-20 01:46:30 +0530

    Frontend homepage initialisation done

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-stone-200 h-20 w-full'>
+        hi there
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navbar from '../components/Navbar.jsx';
+import front from '../assets/front.png';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className='h-screen bg-teal-600 w-full flex items-center'>
+        {/* Content Section */}
+        <div className='w-1/2 p-12 flex flex-col justify-center space-y-6'>
+          <span className='text-white text-5xl font-extrabold font-sans italic leading-tight'>
+          Need Assistance? We're Here to Help!
+          </span>
+          <span className='text-white text-3xl font-bold font-sans italic'>
+          Connect with Our Pet Care Experts Today
+          </span>
+          <input
+            type='text'
+            placeholder='Search doctors,clinics...'
+            className='p-4 rounded-lg bg-white text-gray-800 placeholder-gray-500 shadow-md focus:outline-none focus:ring-2 focus:ring-teal-400'
+          />
+        </div>
+
+        {/* Image Section */}
+        <div className='w-1/2 flex items-center justify-center p-8'>
+          <img src={front} alt="Image of animal" className='w-full h-auto max-w-lg object-cover rounded-lg shadow-lg' />
+        </div>
+      </div>
+      <div className='h-20 bg-gray-200 w-full'></div>
+    </div>
+  );
+}
+
+export default HomePage;
